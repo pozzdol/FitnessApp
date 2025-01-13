bba4b50dbe9814710a581ee21baf1fc8df973c79
pozzdol <fkriachmd@gmail.com>
2025-01-14 01:52:21 +0700

    part 36

---
+++ lib/Screen/DetailedCardScreen.dart
@@ -0,0 +1,154 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class DetailedCardscreen extends StatefulWidget {
+  final String imageUrl;
+  final String name;
+  final String slogan;
+  final int second;
+  final String username;
+  final String usermail;
+
+  DetailedCardscreen({
+    required this.imageUrl,
+    required this.name,
+    required this.slogan,
+    required this.second,
+    required this.username,
+    required this.usermail,
+  });
+
+  @override
+  State<DetailedCardscreen> createState() => _DetailedCardscreenState();
+}
+
+class _DetailedCardscreenState extends State<DetailedCardscreen> {
+  late int _secondRemaining;
+  late bool _isCounting;
+  late bool _excerciseCompleted;
+
+  @override
+  void initState() {
+    super.initState();
+    _secondRemaining = widget.second;
+    _isCounting = false;
+    _excerciseCompleted = false;
+  }
+
+  void _startCounting() {
+    setState(() {
+      _isCounting = true;
+    });
+
+    const oneSec = Duration(seconds: 1);
+    Timer.periodic(oneSec, (timer) {
+      if (_secondRemaining == 0){
+        setState(() {
+          _isCounting = false;
+          _excerciseCompleted = true;
+        });
+      } else{
+        setState(() {
+          _secondRemaining --;
+        });
+      }
+    });
+  }
+
+  Future<void> _loadExcerciseComplicationState() async {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Excercise Details"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Text('Welcome, ${widget.username}'),
+          SizedBox(
+            height: 10,
+          ),
+          Text('Welcome, ${widget.usermail}'),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Image.network(widget.imageUrl),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Text(
+                  widget.name,
+                  style: const TextStyle(
+                      fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  widget.slogan,
+                  style: const TextStyle(fontSize: 18),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                _isCounting
+                    ? Column(
+                        children: [
+                          CircularProgressIndicator(
+                            value: (_secondRemaining / widget.second),
+                            strokeWidth: 10,
+                            backgroundColor: Colors.grey,
+                            valueColor:
+                                const AlwaysStoppedAnimation(Colors.blue),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            '${_secondRemaining} seconds',
+                            style: const TextStyle(fontSize: 24),
+                          )
+                        ],
+                      )
+                    : const SizedBox(
+                        height: 16.0,
+                      ),
+                _excerciseCompleted
+                    ? Column(
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('Reset'),
+                          ),
+                          const SizedBox(
+                            height: 20.0,
+                          ),
+                          Text(
+                            'Excercise Complited',
+                            style: TextStyle(fontSize: 16),
+                          )
+                        ],
+                      )
+                    : ElevatedButton(
+                        onPressed: () {
+                          _startCounting();
+                        },
+                        child: const Text('Start Counting'),
+                      )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/Screen/dashboardScreen.dart
+++ lib/Screen/dashboardScreen.dart
@@ -1,35 +1,123 @@
+import 'dart:convert';
+import 'package:fitness/Screen/DetailedCardScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class DashboardScreen extends StatefulWidget {
   final String name;
   final String email;
-  const DashboardScreen({required this.name, required this.email});
+  const DashboardScreen({required this.name, required this.email, Key? key})
+      : super(key: key);
 
   @override
   State<DashboardScreen> createState() => _DashboardScreenState();
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  List<dynamic> exercises = [];
+  String baseUrl = 'http://10.0.2.2/fitness';
+  bool isLoading = true; // Tambahkan indikator loading
+
+  Future<void> _fetchExercises() async {
+    try {
+      final apiUrl = '$baseUrl/ex.php';
+      final response = await http.get(Uri.parse(apiUrl));
+
+      if (response.statusCode == 200) {
+        setState(() {
+          exercises = jsonDecode(response.body);
+          isLoading = false; // Set loading selesai
+        });
+      } else {
+        throw Exception('Failed to load exercises');
+      }
+    } catch (e) {
+      setState(() {
+        isLoading = false; // Set loading selesai meski gagal
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Error fetching exercises: $e'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchExercises();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Dashboard"),
-        actions: [
-          IconButton(onPressed: (){
-            Navigator.pop(context);
-          }, icon: const Icon(Icons.exit_to_app))
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Welcome ${widget.name}", style: const TextStyle(fontSize: 30),),
-            Text("Welcome ${widget.email}"),
+        appBar: AppBar(
+          title: const Text('Dashboard'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.exit_to_app),
+            ),
           ],
         ),
-      ),
-    );
+        body: exercises.isEmpty
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : Column(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text('Welcome, ${widget.name}'),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text("Excercise List"),
+                  Expanded(
+                    child: ListView.builder(
+                        itemCount: exercises.length,
+                        itemBuilder: (context, index) {
+                          final exercise = exercises[index];
+                          final imageUrl = '$baseUrl/${exercise['image_url']}';
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => DetailedCardscreen(
+                                        imageUrl: imageUrl,
+                                        name: exercise['name'],
+                                        slogan: exercise['slogan'],
+                                        second: int.parse(exercise['second']),
+                                        username: widget.name,
+                                        usermail: widget.email,
+                                    ),
+                                  ),
+                                );
+                              },
+                              child: Card(
+                                child: ListTile(
+                                  leading: CircleAvatar(
+                                    radius: 30,
+                                    backgroundImage: NetworkImage(imageUrl),
+                                  ),
+                                  title: Text(exercise['name']),
+                                  subtitle: Text(exercise['slogan']),
+                                  trailing: Text('${exercise['second']}s'),
+                                ),
+                              ),
+                            ),
+                          );
+                        }),
+                  ),
+                ],
+              ));
   }
 }
